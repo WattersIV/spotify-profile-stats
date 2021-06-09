a17d6b1922b5bd2787ea7ecb2d91a9a14f89907a
Bill Watters <billywatters13@gmail.com>
2021-06-09 19:31:29 -0400

    Config env vars and redirect uris for login route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ var request = require("request"); // "Request" library
 var cors = require("cors");
 var querystring = require("querystring");
 var cookieParser = require("cookie-parser");
+require("dotenv").config({ path: __dirname + "/.env" });
 
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
