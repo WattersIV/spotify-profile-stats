b808e37cb1b3f50b891c2e7210604293827ffde3
Bill Watters <billywatters13@gmail.com>
2021-06-15 15:13:01 -0400

    Add basic styling for artists and songs ppages and working buttons to change time length

--- client/src/components/artists/Artists.jsx
+++ client/src/components/artists/Artists.jsx
@@ -4,10 +4,16 @@ import { getTopListening } from "../../api/spotify";
 export default function Artists() {
   const [timeRange, setTimeRange] = useState("long_term");
   const [artists, setArtists] = useState(null);
+
   useEffect(() => {
     // fetch based on new time range
-    console.log(timeRange);
+    async function getArtists() {
+      const artistsData = await getTopListening(timeRange, "artists", 50);
+      setArtists(artistsData);
+    }
+    getArtists();
   }, [timeRange]);
+
   useEffect(() => {
     // First render needs to get the long term tracks on state change fetch a new length
     async function getArtists() {
@@ -16,41 +22,64 @@ export default function Artists() {
     }
     getArtists();
   }, []);
+
   return (
     <div className="t-a">
-      <h1>Top Artists</h1>
-      <ul className="t-a__selectors">
-        <li className="t-a__selectors--item">
-          <button
-            onClick={() => {
-              setTimeRange("long_term");
-            }}
-            className="rm-button-style"
-          >
-            All Time
-          </button>
-        </li>
-        <li className="t-a__selectors--item">
-          <button
-            onClick={() => {
-              setTimeRange("medium_term");
-            }}
-            className="rm-button-style"
-          >
-            6 Months
-          </button>
-        </li>
-        <li className="t-a__selectors--item">
-          <button
-            onClick={() => {
-              setTimeRange("short_term");
-            }}
-            className="rm-button-style"
-          >
-            4 Weeks
-          </button>
-        </li>
-      </ul>
+      <div className="t-a__titles">
+        <>
+          <h1>Top Artists</h1>
+          <ul className="t-a__selectors">
+            <li className="t-a__selectors--item">
+              <button
+                onClick={() => {
+                  setTimeRange("long_term");
+                }}
+                className="rm-button-style"
+              >
+                All Time
+              </button>
+            </li>
+            <li className="t-a__selectors--item">
+              <button
+                onClick={() => {
+                  setTimeRange("medium_term");
+                }}
+                className="rm-button-style"
+              >
+                6 Months
+              </button>
+            </li>
+            <li className="t-a__selectors--item">
+              <button
+                onClick={() => {
+                  setTimeRange("short_term");
+                }}
+                className="rm-button-style"
+              >
+                4 Weeks
+              </button>
+            </li>
+          </ul>
+        </>
+      </div>
+      {artists && (
+        <ul className="artists-list">
+          {artists.items.map((artist) => {
+            return (
+              <li key={artist.name} className="artist">
+                <div className="artist__img-container">
+                  <img
+                    src={artist.images[2].url}
+                    alt={`${artist.name} picture`}
+                    className="artist__img-container--picture"
+                  />
+                </div>
+                <h5>{artist.name}</h5>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
--- client/src/components/tracks/Tracks.jsx
+++ client/src/components/tracks/Tracks.jsx
@@ -6,7 +6,11 @@ export default function Tracks() {
   const [tracks, setTracks] = useState(null);
   useEffect(() => {
     // fetch based on new time range
-    console.log(timeRange);
+    async function getTracks() {
+      const tracksData = await getTopListening(timeRange, "tracks", 50);
+      setTracks(tracksData);
+    }
+    getTracks();
   }, [timeRange]);
 
   useEffect(() => {
@@ -17,41 +21,71 @@ export default function Tracks() {
     }
     getTracks();
   }, []);
+  console.log(tracks);
   return (
     <div className="t-a">
-      <h1>Top Tracks</h1>
-      <ul className="t-a__selectors">
-        <li className="t-a__selectors--item">
-          <button
-            onClick={() => {
-              setTimeRange("long_term");
-            }}
-            className="rm-button-style"
-          >
-            All Time
-          </button>
-        </li>
-        <li className="t-a__selectors--item">
-          <button
-            onClick={() => {
-              setTimeRange("medium_term");
-            }}
-            className="rm-button-style"
-          >
-            6 Months
-          </button>
-        </li>
-        <li className="t-a__selectors--item">
-          <button
-            onClick={() => {
-              setTimeRange("short_term");
-            }}
-            className="rm-button-style"
-          >
-            4 Weeks
-          </button>
-        </li>
-      </ul>
+      <div className="t-a__titles">
+        <>
+          <h1>Top Tracks</h1>
+          <ul className="t-a__selectors">
+            <li className="t-a__selectors--item">
+              <button
+                onClick={() => {
+                  setTimeRange("long_term");
+                }}
+                className="rm-button-style"
+              >
+                All Time
+              </button>
+            </li>
+            <li className="t-a__selectors--item">
+              <button
+                onClick={() => {
+                  setTimeRange("medium_term");
+                }}
+                className="rm-button-style"
+              >
+                6 Months
+              </button>
+            </li>
+            <li className="t-a__selectors--item">
+              <button
+                onClick={() => {
+                  setTimeRange("short_term");
+                }}
+                className="rm-button-style"
+              >
+                4 Weeks
+              </button>
+            </li>
+          </ul>
+        </>
+      </div>
+      {tracks && (
+        <ul className="tracks">
+          {tracks.items.map((track) => {
+            return (
+              <li className="track">
+                <div className="track__image-wrapper">
+                  <img
+                    src={track.album.images[1].url}
+                    alt={`${track.album.name} cover`}
+                  />
+                </div>
+                <div className="track__info">
+                  <h3>{track.name}</h3>
+                  <div className="track__info--artist">
+                    <h5>
+                      {" "}
+                      {`${track.album.artists[0].name} - ${track.album.name}`}{" "}
+                    </h5>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
