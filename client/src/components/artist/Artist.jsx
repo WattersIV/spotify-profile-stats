d8fd675eab23214892e4680f569c3262fab7077f
Bill Watters <billywatters13@gmail.com>
2021-06-24 23:04:57 -0400

    Add followers pop score and picture to artist page

--- client/src/components/artist/Artist.jsx
+++ client/src/components/artist/Artist.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getArtist } from "../../api/spotify";
+import Navbar from "../Navbar";
+import { useParams } from "react-router";
 
+export default function Artist() {
+  const [artistInfo, setArtistInfo] = useState(null);
+  const { id } = useParams();
 
-export default function Artist () {
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getArtist(id);
+      setArtistInfo(data);
+    }
+    fetchData();
+  }, []);
 
+  console.log(artistInfo);
   return (
-    <h1>HELLO FROM ARITST ID PAGE</h1>
-  )
+    <>
+      <div className="sidebar">
+        <Navbar />
+      </div>
+      <div className="main">
+        {artistInfo && (
+          <div className="artist-info">
+            <div className="artist-info__img-container">
+              <img
+                src={artistInfo.images[2].url}
+                alt={`${artistInfo.name} picture`}
+                className="artist-info__img-container--picture"
+              />
+            </div>
+            <h1 className="center">{artistInfo.name}</h1>
+            <div className="artist-info__stats">
+              <div className="artist-info__section">
+                <h2>{artistInfo.popularity}</h2>
+                <h3 className="title-text">Popularity</h3>
+              </div>
+              <div className="artist-info__section">
+                <h2>{artistInfo.followers.total}</h2>
+                <h3 className="title-text">Followers</h3>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
