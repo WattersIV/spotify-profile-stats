b544738dec2d99c00c19df22febed279bd4fe02b
Bill Watters <billywatters13@gmail.com>
2021-06-26 15:24:28 -0400

    Artist page done

--- client/src/components/artist/Artist.jsx
+++ client/src/components/artist/Artist.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { getArtist } from "../../api/spotify";
 import Navbar from "../Navbar";
+import { useWindowWidth } from "@react-hook/window-size";
 import { useParams } from "react-router";
 
 export default function Artist() {
   const [artistInfo, setArtistInfo] = useState(null);
   const { id } = useParams();
+  const width = useWindowWidth();
 
   useEffect(() => {
     async function fetchData() {
@@ -14,17 +16,6 @@ export default function Artist() {
     }
     fetchData();
   }, []);
-
-  const humanReadableNum = (num) => {
-    let newNum = "";
-    num = String(num);
-    console.log("post str", num);
-    for (let idx = 0; idx < num.length; idx++) {
-      if (idx % 3 === 0 && idx !== 0) newNum += ",";
-      newNum += num[idx];
-    }
-    return newNum;
-  };
 
   return (
     <>
@@ -36,7 +27,11 @@ export default function Artist() {
           <div className="artist-info">
             <div className="artist-info__img-container">
               <img
-                src={artistInfo.images[2].url}
+                src={
+                  width > 770
+                    ? artistInfo.images[0].url
+                    : artistInfo.images[1].url
+                }
                 alt={`${artistInfo.name} picture`}
                 className="artist-info__img-container--picture"
               />
@@ -48,7 +43,7 @@ export default function Artist() {
                 <h3 className="title-text">Popularity</h3>
               </div>
               <div className="artist-info__section">
-                <h2>{humanReadableNum(artistInfo.followers.total)}</h2>
+                <h2>{artistInfo.followers.total.toLocaleString()}</h2>
                 <h3 className="title-text">Followers</h3>
               </div>
             </div>
