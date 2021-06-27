bcf74dd1439a282a1ebb0d1bde699f4cea5bc299
Bill Watters <billywatters13@gmail.com>
2021-06-27 00:36:45 -0400

    Add lazy loading for each page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Routes from "./Routes";
-import Navbar from "./Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 export default function Profile() {
   return (
     <Router>
--- client/src/components/Routes.jsx
+++ client/src/components/Routes.jsx
@@ -1,29 +1,21 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import User from "./user/User";
-import Artists from "./artists/Artists";
-import Tracks from "./tracks/Tracks";
-import Artist from "./artist/Artist";
-import Track from "./track/Track";
+const User = React.lazy(() => import("./user/User"));
+const Artists = React.lazy(() => import("./artists/Artists"));
+const Tracks = React.lazy(() => import("./tracks/Tracks"));
+const Artist = React.lazy(() => import("./artist/Artist"));
+const Track = React.lazy(() => import("./track/Track"));
 
 export default function Routes() {
   return (
-    <Switch>
-      <Route path="/artist/:id">
-        <Artist />
-      </Route>
-      <Route path="/artists">
-        <Artists />
-      </Route>
-      <Route path="/track/:id">
-        <Track />
-      </Route>
-      <Route path="/tracks">
-        <Tracks />
-      </Route>
-      <Route path="/">
-        <User />
-      </Route>
-    </Switch>
+    <React.Suspense fallback={"Loading"}>
+      <Switch>
+        <Route path="/artist/:id" component={Artist} />
+        <Route path="/artists" component={Artists} />
+        <Route path="/track/:id" component={Track} />
+        <Route path="/tracks" component={Tracks} />
+        <Route path="/" component={User} />
+      </Switch>
+    </React.Suspense>
   );
 }
